7c281dd62a727ed835b661b0c0d358281c8a7003
Emmanuel Nambu <emmanuelnambu@outlook.com>
2025-05-18 15:20:57 +0000

    New_Fix

--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -1,6 +1,4 @@
 import { TrendingDownIcon, TrendingUpIcon } from "lucide-react"
-
-import { Badge } from "@/components/ui/badge"
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Component as BarChartDefault } from "@/components/chart-bar-default"
 import { Component as LineGraph} from "./chart-line-label-custom";
@@ -10,14 +8,15 @@ export function SectionCards() {
     <div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6">
       <Card className="@container/card">
         <CardHeader className="relative">
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">$1,250.00</CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
-              <TrendingUpIcon className="size-3" />
-              +12.5%
-            </Badge>
-          </div>
+          <CardDescription className="flex items-center justify-between gap-2 w-full">
+            Total Revenue
+            <div className="flex gap-1 rounded-lg text-xs bg-[#e2e8f9] text-[#243056] border border-[#3b4a6b] px-2 py-0.5">
+              <TrendingDownIcon className="size-3" />
+              +12%
+            </div>
+          </CardDescription>
+          <CardTitle className="@[250px]/card:text-[60px] mt-4 text-2xl font-semibold tabular-nums">$1,250.00</CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
@@ -28,14 +27,15 @@ export function SectionCards() {
       </Card>
       <Card className="@container/card">
         <CardHeader className="relative">
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">1,234</CardTitle>
-          <div className="absolute right-4 top-4">
-            <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
+          <CardDescription className="flex items-center justify-between gap-2 w-full">
+            New Customers
+            <div className="flex gap-1 rounded-lg text-xs bg-[#ebf9e2] text-[#4a6c41] border border-[#85db57] px-2 py-0.5">
               <TrendingDownIcon className="size-3" />
               -20%
-            </Badge>
-          </div>
+            </div>
+          </CardDescription>
+          <CardTitle className="@[250px]/card:text-[60px] mt-4 text-2xl font-semibold tabular-nums">1,234</CardTitle>
+          
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
